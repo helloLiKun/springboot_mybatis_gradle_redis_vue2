b452d7d57472c1f393c6bcf05f482eb52e091c87
Administrator <13923813589@163.com>
2018-08-18 20:39:23 +0800

    #

--- src/main/java/example/user/controller/UserController.java
+++ src/main/java/example/user/controller/UserController.java
@@ -1,10 +1,15 @@
 package example.user.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import example.user.entity.User;
 import example.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -14,14 +19,20 @@ import java.util.List;
 @Controller
 public class UserController {
     @RequestMapping("/test")
-    public String userList(){
+    public String userList(ModelMap map){
         List<User> list=userService.getList();
-        for(User user:list){
-            System.out.println("username--->"+user.getName());
-        }
+        map.put("users",list);
+        map.put("userJSON",JSON.toJSONString(new User(), SerializerFeature.WriteMapNullValue));
+        map.put("usersJSON", JSON.toJSONString(list));
         return "user/list";
     }
 
+    @RequestMapping("/userSubmit")
+    @ResponseBody
+    public  String userSubmit(@RequestBody User user){
+        System.out.println("user:"+user.toString());
+        return "";
+    }
 
     @Autowired
     UserService userService;
