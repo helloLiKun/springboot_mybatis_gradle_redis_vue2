bbb86886d1514865b01b822012316363dd6a8656
Administrator <13923813589@163.com>
2018-10-14 23:26:01 +0800

    #

--- src/main/resources/static/lib/search-select/js/search-select.js
+++ src/main/resources/static/lib/search-select/js/search-select.js
@@ -1,7 +1,9 @@
 /**
  * Created by Administrator on 2018/9/24 0024.
  */
-
+$("body").click(function(){
+    $('.search-select-cls > div').css('display','none');
+});
 function  SearchSelect(data) {
     var liHeight=30;
     var self = this;
@@ -9,28 +11,46 @@ function  SearchSelect(data) {
         console.log('id不能为空');
         return;
     }
-    $('#' + data.id).append("<input><ul></ul>");
+    $('#' + data.id).append("<input><div><table></table></div>");
     if (!data.options) {
         console.log('选项为空')
     }
     var id = data.id;
     var options = data.options;
     var divElement = $('#' + id);
-    var inputElement = $('#' + id + ' input');
-    var ulElement=$('#'+id+' ul');
-    var lis =$('#'+id+' ul > li');
-    var flushLis=function () {
-        ulElement.html('');
-        ulElement.height(0);
+    var inputElement = $('#' + id + ' > input');
+    var optionsElement=$('#'+id+' > div');
+    var tabElement=$('#'+id+' > div > table');
+    var trElement=$('#'+id+' > div > table > tr');
+    var width=divElement.width();
+    divElement.click(function (e) {
+        optionsElement.css('display','block');
+        e.cancelBubble=true;
+        e.stopPropagation();
+        $('.search-select-cls').each(function () {
+            var thisid=$(this).attr("id");
+            if(thisid!=id){
+                $('#'+thisid+' > div').css('display','none');
+            }
+        })
+    })
+    var flushTrs=function () {
+        tabElement.html('');
+        tabElement.height(0);
         if(data.options!=null && data.options.length>0){
             for(var i=0;i<data.options.length;i++){
                 var option=data.options[i];
-                ulElement.append("<li value='"+option.val+"'>"+option.text+"</li>")
+                tabElement.append("<tr><td click='"+trclick()+"' value='"+option.val+"'>"+option.text+"</td></tr>")
             }
-            lis.width(ulElement.width());
-            var liNum=data.options.length>5?5:data.options.length;
-            ulElement.height(liNum*liHeight+15);
+            optionsElement.width(width);
+            tabElement.width(width-2);
+            trElement.width(width-2);
+            var trNum=data.options.length>5?5:data.options.length;
+            optionsElement.height(trNum*liHeight+15);
         }
     }
-    flushLis();
+    function trclick(e) {
+        console.log('--click  val---------->'+e)
+    }
+    flushTrs();
 }
